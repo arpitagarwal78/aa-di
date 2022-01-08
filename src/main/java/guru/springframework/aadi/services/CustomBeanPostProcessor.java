c09eed4546631d7f4ce1dca41a3ef9c6cf941397
Arpit Agarwal <arpitagarwal0708@gmail.com>
2022-01-08 20:18:42 +0530

    - LifeCycleDemoBean - SpringInterfaces to work with spring lifecycle events - CustomBeanPostProcessor - Inspect all spring beans before they are initialised and after initialised

---
+++ src/main/java/guru/springframework/aadi/services/CustomBeanPostProcessor.java
@@ -0,0 +1,28 @@
+package guru.springframework.aadi.services;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+// this is lifecycle methods for spring context beans
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+
+        if(bean instanceof LifeCycleDemoBean){
+            ((LifeCycleDemoBean) bean).beforeInit();
+        }
+
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if(bean instanceof LifeCycleDemoBean){
+            ((LifeCycleDemoBean) bean).afterInit();
+        }
+
+        return bean;
+    }
+}
