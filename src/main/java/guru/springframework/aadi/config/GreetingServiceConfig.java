8f033f353d4535d53cd98ac4b7c364bb1c513dd3
Arpit Agarwal <arpitagarwal0708@gmail.com>
2022-01-09 11:20:19 +0530

    XML Based Configuration in SpringBoot

--- src/main/java/guru/springframework/aadi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/aadi/config/GreetingServiceConfig.java
@@ -8,15 +8,13 @@ import guru.springframework.aadi.services.ConstructorGreetingService;
 import guru.springframework.aadi.services.I18nEnglishGreetingService;
 import guru.springframework.aadi.services.I18nSpanishGreetingService;
 import guru.springframework.aadi.services.PrimaryGreetingService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 
+@ImportResource("classpath:aadi-config.xml")
 @Configuration // Spring will look into this class to see configuration
 public class GreetingServiceConfig {
 
-    @Bean
+    // no need to add Bean as XML file loads that for us
     ConstructorGreetingService constructorGreetingService(){ // this becomes the name of bean
         return new ConstructorGreetingService();
     }
